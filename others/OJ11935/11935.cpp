c0d0e33095ab3ccac8ea0657660163482d56af13
SwampTG <adriansoareslps@gmail.com>
2023-01-02 21:18:04 -0300

    Solution for OJ11935 using divide and conquer
    Co-authored-by: Paulo Vitor Abi <PauloAbiAcl@users.noreply.github.com>

--- others/OJ11935/11935.cpp
+++ others/OJ11935/11935.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 const double N_MAX = 0.3;
-const int N_OPS = 55;
+const int N_OPS = 52;
 
 const string CONS_IN = "Fuel";
 const string LEAK_IN = "Leak";
@@ -32,16 +32,16 @@ int e_size;
 double model(double vi, int ops[2], double k1, double k0) {
   double vf = 0.0;
   double kf = k1 - k0; 
-  cout << "model: " << " d: " << kf;
+  //cout << "model: " << " d: " << kf;
   double consumption = (double) ops[CONSUMPTION] * kf / 100.0;
   double leak = (double) ops[LEAK] * kf;
 
-  cout << " cons: " << consumption;
-  cout << " leak: " << leak;
+  //cout << " cons: " << consumption;
+  //cout << " leak: " << leak;
 
   vf = vi - consumption - leak;
 
-  cout << " vf: " << vf << '\n';
+  //cout << " vf: " << vf << '\n';
   return vf; 
 }
 
@@ -52,11 +52,10 @@ pair<double, bool> process(double vi) {
   bool reached = false;
   
   for (int i = 0; i < e_size; ++i) {
-    if (vf < 0.0) break;
-
-    //cout << "Event: " << evs[i].ev << " km0: " << k0 << " km1 " << evs[i].km << " Consumption " << ops[CONSUMPTION] << " Leaks: " << ops[LEAK] << '\n';
+    // cout << "Event: " << evs[i].ev << " km0: " << k0 << " km1 " << evs[i].km << " Consumption " << ops[CONSUMPTION] << " Leaks: " << ops[LEAK] << '\n';
 
     vf = model(vf, ops, evs[i].km, k0);
+    if (vf < 0.0) break;
 
     if (evs[i].ev == CONSUMPTION) ops[CONSUMPTION] = evs[i].cons;
     if (evs[i].ev == FIX) ops[LEAK] = 0;
@@ -72,25 +71,25 @@ pair<double, bool> process(double vi) {
   return make_pair(vf, reached);
 }
 
-double search_ans(double total, double l = 0.0, double r = -1.0) {
-  if (r == -1.0) r = N_MAX * total + N_OPS * total;
-  cout << "Left: " << l << " Right: " << r;
-
+double search_ans(double l, double r) {
   double vi = (l + r) / 2;
 
-  cout << " mid: " << vi << '\n';
+  // cout << "Left: " << l << " Right: " << r << " mid: " << vi << '\n';
+  
+  if (fabs(r - l) <= 10e-9) return vi;
+
   auto res = process(vi);
-  cout << "Reached: " << res.second << "Vol diff: " << res.first << '\n';
-  if (res.second == true && fabs(res.first) <= 10e-4) return vi;
-  else if (res.second == true && res.first > 0) {
-    return search_ans(total, l, vi);
+
+  // cout << "Reached: " << res.second << " Vol diff: " << res.first << '\n';
+  if (res.second) {
+    return search_ans(l, vi);
   } else {
-    return search_ans(total, vi, r);
+    return search_ans(vi, r);
   }
 }
 
-void solve(double total) {
-  cout << search_ans(total) << '\n';
+void solve() {
+  cout << search_ans(0, 10000.0) << '\n';
 }
 
 int main (void) {
@@ -107,11 +106,13 @@ int main (void) {
       ++next;
       inpstring >> evs[next].km >> event;
 
-      cout << "Km: " << evs[next].km << "Event: " << event << '\n';
+      //cout << "Km: " << evs[next].km << "Event: " << event << '\n';
 
       if (event == CONS_IN) evs[next].ev = CONSUMPTION; //, cout << "is cons";
       if (event == LEAK_IN) evs[next].ev = LEAK; //, cout << "is leak";
-      if (event == STATION_IN) evs[next].ev = FILL ; //, cout << "is Station";
+      if (event == STATION_IN) {
+        evs[next].ev = FILL ; //, cout << "is Station";
+      } 
       if (event == FIX_IN) evs[next].ev = FIX; //, cout << "is Mechanic";
       if (event == GOAL_IN) evs[next].ev = GOAL; //, cout << "is goal";
       // cout << '\n';
@@ -119,7 +120,7 @@ int main (void) {
       if (event == CONS_IN || event == STATION_IN) inpstring >> trash;
       if (event == CONS_IN) {
         inpstring >> evs[next].cons;
-        //cout << "Consumo: " << evs[next].cons << '\n';
+        // cout << "Consumo: " << evs[next].cons << '\n';
       }
 
       if (event == GOAL_IN) break;
@@ -127,9 +128,10 @@ int main (void) {
     } while(getline(cin, inp_line));
 
     e_size = next + 1;
-    //cout << " number of evs: " << e_size << '\n';
+    // cout << " number of evs: " << e_size << '\n';
 
-    solve (evs[next].km);
+    solve ();
+    // cout << "\n ----------------------------------- \n";
   }
 
   return 0;
